543e128798d2891ad4509b6d49097c26e1d90d40
unknown <devuriah@gmail.com>
2022-06-30 12:18:05 +0000

    Firebase Changes

--- src/components/config.js
+++ src/components/config.js
@@ -1,9 +1,11 @@
+const p = process.env;
+
 export const firebaseConfig = {
-    apiKey: "AIzaSyDdL2ShSpVpFfrC3GEU-qQHzYs567BtdCs",
-    authDomain: "omozon-4aee2.firebaseapp.com",
-    projectId: "omozon-4aee2",
-    storageBucket: "omozon-4aee2.appspot.com",
-    messagingSenderId: "295795340465",
-    appId: "1:295795340465:web:987cad456987d2a393de90"
+    apiKey: p.REACT_APP_API_KEY,
+    authDomain: p.REACT_APP_AUTH_DOMAIN,
+    projectId: p.REACT_APP_PROJECT_ID,
+    storageBucket: p.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: p.REACT_APP_MESSAGING_SENDER_ID,
+    appId: p.REACT_APP_APP_ID
 };
 
